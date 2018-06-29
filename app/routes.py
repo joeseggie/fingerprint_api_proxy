d7db6e5ad61196583da29d1b7b2dbd60358a875b
Joseph Serunjogi <joseph.serunjogi@hotmail.com>
2018-06-29 15:59:53 +0300

    Route to match fingerprint

--- app/routes.py
+++ app/routes.py
@@ -1,8 +1,19 @@
 from app import app
-from flask import jsonify
+from flask import jsonify, request
+from app.soapclientbuilder import SoapClientBuilder
 
-@app.route('/')
-def index():
-    environment = app.config['ENV']
-    fingerprint_wsdl = app.config['FINGERPRINT_WSDL']
-    return jsonify({'Environment': environment, 'Fingerprint WSDL': fingerprint_wsdl})
+@app.route('/fingerprintapiproxy/api/v1.0/match', methods=['POST'])
+def match():
+    soapclient = SoapClientBuilder()
+    
+    data = request.get_json()
+    
+    msisdn = data['Msisdn']
+    probe_minutiae = data['ProbeMinutiae']
+    candidate_template = data['CandidateTemplate']
+
+    soap_request_body = soapclient.build_request_body(msisdn=msisdn, probe_minutiae=probe_minutiae, candidate_template=candidate_template)
+    soap_response = soapclient.send_request(soap_request_body)
+    parsed_soap_response = soapclient.parse_response(soap_response)
+
+    return jsonify(parsed_soap_response),201
